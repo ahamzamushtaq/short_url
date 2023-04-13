a12f11983721b2e585bde9cfa32044844865e7a4
Ameer Hamza Mushtaq <hamzamushtaq3071@gmail.com>
2023-04-14 03:33:39 +0500

    APP to generate shorl url

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :urls, only: [:index, :new, :create, :show]
+  resources :users, only: [:index, :new, :create, :show]
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
